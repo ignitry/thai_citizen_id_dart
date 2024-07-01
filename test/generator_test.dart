272f706dfb28ceb2289748c8010d0b8b34bdd655
wittawasw <start@wittawasw.com>
2024-07-02 05:28:49 +0700

    add tests

--- test/generator_test.dart
+++ test/generator_test.dart
@@ -1,5 +1,22 @@
 import 'package:test/test.dart';
+import 'package:thai_citizen_id/thai_citizen_id.dart';
+import 'package:thai_citizen_id/generator.dart';
 
 void main() {
-  group('Generator', () {});
+  group('Generator', () {
+    test('should generate valid Thai Citizen ID', () {
+      String generatedID = Generator.generateThaiCitizenID();
+      ThaiCitizenID thaiID = ThaiCitizenID(generatedID);
+
+      expect(thaiID.valid(), isTrue);
+      expect(thaiID.errors, isEmpty);
+    });
+
+    test('should have 13 digits', () {
+      String generatedID = Generator.generateThaiCitizenID();
+
+      expect(generatedID.length, equals(13));
+      expect(RegExp(r'^\d{13}$').hasMatch(generatedID), isTrue);
+    });
+  });
 }
